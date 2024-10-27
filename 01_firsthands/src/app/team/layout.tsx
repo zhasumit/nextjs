0fe88ba879e63817bf72b951a683a0dfa4c3ea97
zhasumit <sumitzha2057@gmail.com>
2024-10-27 17:12:05 +0545

    routes and metadata

--- 01_firsthands/src/app/team/layout.tsx
+++ 01_firsthands/src/app/team/layout.tsx
@@ -1,11 +1,17 @@
+import { Metadata } from "next";
+
 export default function lay({ children }) {
     return (
         <>
             <h1>
-                layout section
+                TEAM layout section
             </h1>
             {children}
         </>
     )
 
 }
+
+export const metadata: Metadata = {
+    title: "Hi from TEAM layout",
+};
