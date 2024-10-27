1168be0120173e957e43a762b0b11c5ffa9602fe
zhasumit <sumitzha2057@gmail.com>
2024-10-27 09:34:05 +0545

    firsthands

---
+++ 01_firsthands/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div className="h1">Home</div>
+  );
+}
---
+++ 01_firsthands/src/app/team/layout.tsx
@@ -0,0 +1,11 @@
+export default function lay({ children }) {
+    return (
+        <>
+            <h1>
+                layout section
+            </h1>
+            {children}
+        </>
+    )
+
+}
